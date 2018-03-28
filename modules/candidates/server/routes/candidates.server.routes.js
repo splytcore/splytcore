7358e2235d1e2fb3fa12ea84bec1fd3f789283ec
Mona Lisa <scottkimdmc@gmail.com>
2018-03-28 09:02:26 -0700

    added some stupid doc

--- modules/candidates/server/routes/candidates.server.routes.js
+++ modules/candidates/server/routes/candidates.server.routes.js
@@ -9,39 +9,50 @@ let candidates = require('../controllers/candidates.server.controller')
 module.exports = function(app) {
   // Candidates Routes
   
+  //@desc candidate checks in from tablet
   app.route('/api/checkin')
     .post(candidates.checkin) 
             
+  //@desc register from our tablet
   app.route('/api/register/MOBILE')
     .post(candidates.uploadImageResume, candidates.mergeImagesToPDF, candidates.register)   
              
+  //@desc register from our web site
   app.route('/api/register/WEB')
     .post(candidates.uploadDocResume, candidates.register)       
 
+  //@desc find candidates by email
   app.route('/api/candidateByEmail/:email')
     .get(candidates.findByEmail)   
 
-  //find candidates by email, last name, sms
+  //@desc find candidates by email, last name, sms
   //i.e. /api/findCandidate?q=john@gmail.com
   //i.e. /api/findCandidate?q=smith
   //i.e. /api/findCandidate?q=81812345678
   app.route('/api/findCandidate/:search')
     .get(candidates.findCandidate)   
 
+  //@desc validate phone through twilio validation
   app.route('/api/validatePhone/:phone')
     .get(candidates.validatePhone)    
 
+  //@desc return all candidates 
+  //you can append addition parameters to filter
+  //i.e. ?stage=REGISTERED&registeredFrom=WEB
   app.route('/api/candidates')
     .get(candidates.list)    
 
-  //returns list of enum all
+  //@desc returns list of ALL enum values
+  //each enum key has an array of values
+  //i.e. {registeredFrom: ['WEB', 'MOBILE'], stages: ['REGISTERED','QUEUE',...]}
   app.route('/api/enum/candidates')
     .get(candidates.listAllEnumValues)    
   
-  //returns list of enum values according to model field
+  //@desc returns list of enum values according to model field
   app.route('/api/enum/candidates/:field')
     .get(candidates.listEnumValues)    
     
+  //@desc for specific candidate
   app.route('/api/candidates/:candidateId').all(candidatesPolicy.isAllowed)
     .get(candidates.read)
     .put(candidates.update)
