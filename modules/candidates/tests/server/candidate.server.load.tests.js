931c25cb01643e83b656133702a92509fbb7a665
Mona Lisa <scottkimdmc@gmail.com>
2018-05-10 13:51:34 -0700

    updated form

--- modules/candidates/tests/server/candidate.server.load.tests.js
+++ modules/candidates/tests/server/candidate.server.load.tests.js
@@ -1,227 +1,215 @@
-'use strict'
+// 'use strict'
 
-// let should = require('should')
-
-
-const path = require('path')
-// const express = require(path.resolve('./config/lib/express'))
-const express = require('express')
-
-const random = require('random-name')
-const async = require('async')
-const should = require('should')
-
-const request = require('superagent')
-const cookieParser = require('cookie-parser');
-const cookie = require('cookie');
-
-/**
- * Globals
- */
-let app, 
-    agent, 
-    credentials, 
-    user, 
-    candidate, 
-    positions, 
-    cookieStr, 
-    // baseURL = 'http://54.241.231.143:3000/'               
-    baseURL = 'http://10.0.1.65:3000/'
-
-/**
- * This module routes tests
- */
-describe('Candidate Load testing', () => {
+// // let should = require('should')
 
 
-  before((done) => {    
+// const path = require('path')
+// // const express = require(path.resolve('./config/lib/express'))
+// const express = require('express')
 
-    // Sign in
-    credentials = {
-      email: 'skim@bernsinc.com',
-      password: 'g'
-    }
+// const random = require('random-name')
+// const async = require('async')
+// const should = require('should')
 
-    let url = baseURL + 'api/auth/signin'
+// const request = require('superagent')
+// const cookieParser = require('cookie-parser');
+// const cookie = require('cookie');
+
+
+// /**
+//  * Globals
+//  */
+// let app, 
+//     agent, 
+//     credentials, 
+//     user, 
+//     candidate, 
+//     positions, 
+//     cookieStr, 
+//     // baseURL = 'http://54.241.231.143:3000/'               
+//     baseURL = 'http://10.0.1.65:3000/'
+
+// /**
+//  * This module routes tests
+//  */
+// describe('Candidate Load testing', () => {
+
+
+//   before((done) => {    
+
+//     // Sign in
+//     credentials = {
+//       email: 'skim@bernsinc.com',
+//       password: 'g'
+//     }
+
+//     let url = baseURL + 'api/auth/signin'
     
-    request
-      .post(url)
-      .send(credentials)    
-      .end((err, result) => {
-        if (err) {        
-          console.log('error signin in') 
-          console.log(err)
-          return done(err)
-        }              
-        let setcookie = result.headers["set-cookie"]           
-        console.log('cookie to string: ' + setcookie)
-        let cookies = cookie.parse(setcookie.toString()) 
-        cookieStr = 'sessionId=' + cookies.sessionId
-        console.log(cookieStr)          
-        done()
-      })
+//     request
+//       .post(url)
+//       .send(credentials)    
+//       .end((err, result) => {
+//         if (err) {        
+//           console.log('error signin in') 
+//           console.log(err)
+//           return done(err)
+//         }              
+//         let setcookie = result.headers["set-cookie"]           
+//         console.log('cookie to string: ' + setcookie)
+//         let cookies = cookie.parse(setcookie.toString()) 
+//         cookieStr = 'sessionId=' + cookies.sessionId
+//         console.log(cookieStr)          
+//         done()
+//       })
     
-    // let url = baseURL + 'api/positions'
+//     // let url = baseURL + 'api/positions'
 
-    // console.log(url)    
-    // request
-    //   .get(url)
-    //   .end((err, result) => {
-    //     if (err) {         
-    //       console.log(err)
-    //       return done(err)
-    //     }                      
-    //     console.log('positions length ' + result.body.length)
-    //     positions = result.body                
-    //     done()
-    //   })
+//     // console.log(url)    
+//     // request
+//     //   .get(url)
+//     //   .end((err, result) => {
+//     //     if (err) {         
+//     //       console.log(err)
+//     //       return done(err)
+//     //     }                      
+//     //     console.log('positions length ' + result.body.length)
+//     //     positions = result.body                
+//     //     done()
+//     //   })
 
-  })
+//   })
 
 
-  beforeEach((done) => {    
-    done()
+//   beforeEach((done) => {    
+//     done()
           
-   })
+//    })
 
 
-  // it('should be able to create 1000 registrations', (done) => {    
+//   // it('should be able to create 1000 registrations', (done) => {    
   
-  //   let times = 1000
-  //   let cnt = 0
-  //   let complete = false
+//   //   let times = 1000
+//   //   let cnt = 0
+//   //   let complete = false
 
-  //   let registration = setInterval(function register() {
-  //     let email = Date.now() + '@fakemailfsdfds.com'
-  //     let candidate = { 
-  //       lastName: random.last(), 
-  //       firstName: random.first() , 
-  //       email: email,
-  //       sms: '2136180615',
-  //       position: positions[2],
-  //       registeredFrom: 'WEB'
-  //     }      
-  //     request
-  //       .post(baseURL + 'api/register/WEB')
-  //       .send(candidate)
-  //       .end((err, res) => {
-  //         // Handle signin error        
-  //         console.log(err ? 'create candidate error: ' + err.toString() : '')               
-  //         cnt++
-  //         console.log('counter: ' + cnt)
-  //         if (cnt > times && !complete) {
-  //           complete = true
-  //           clearInterval(registration);
-  //           return done()
-  //         }
-  //       })          
-  //   } ,1000); //1000 for every second
-
-
-  // })
+//   //   let registration = setInterval(function register() {
+//   //     let email = Date.now() + '@fakemailfsdfds.com'
+//   //     let candidate = { 
+//   //       lastName: random.last(), 
+//   //       firstName: random.first() , 
+//   //       email: email,
+//   //       sms: '2136180615',
+//   //       position: positions[2],
+//   //       registeredFrom: 'WEB'
+//   //     }      
+//   //     request
+//   //       .post(baseURL + 'api/register/WEB')
+//   //       .send(candidate)
+//   //       .end((err, res) => {
+//   //         // Handle signin error        
+//   //         console.log(err ? 'create candidate error: ' + err.toString() : '')               
+//   //         cnt++
+//   //         console.log('counter: ' + cnt)
+//   //         if (cnt > times && !complete) {
+//   //           complete = true
+//   //           clearInterval(registration);
+//   //           return done()
+//   //         }
+//   //       })          
+//   //   } ,1000); //1000 for every second
 
 
-  it('should be able to checkin 1000 candidates', (done) => {    
+//   // })
+
+
+//   it('should be able to checkin 1000 candidates', (done) => {    
     
-    new Promise((resolve, reject) => {
-        // let url = baseURL + 'api/candidates?stage=REGISTERED'
-        let url = baseURL + 'api/candidates?stage=QUEUE'        
-        request          
-          .get(url)        
-          .set('Cookie', [cookieStr])
-          .end((err, res) => {
-            if (err) {
-              reject(err)
-            } else {
-              console.log(err ? 'get candidates error: ' + err.toString() : '')               
-              console.log('number of results: ' + res.body.length)              
-              resolve(res.body)
-            }
-          })    
-    })
-    .then((candidates) => {      
-      async.each(candidates, (candidate, callback) => {                                      
-        console.log(candidate.lastName)
-        // request
-        //   .put(baseURL + 'api/candidates/' + candidate._id)
-        //   .set('Cookie', [cookieStr])
-        //   .send({ stage: 'QUEUE' })
-        //   .end((err, result) => {
-        //     if (err) {         
-        //       console.log(err)
-        //       callback(err)
-        //     }          
-        //     console.log(result.body.message)                                            
-        //     callback()
-        //   })                        
-        callback(new Error())
-      }, (err) => {
-        if(err) {
-          return done(err)
-        }             
-        done()
-      })
-    })
-    .catch((err) => {
-      console.log('error man')      
-      console.log(err)      
-    })
+//     new Promise((resolve, reject) => {
+//         // let url = baseURL + 'api/candidates?stage=REGISTERED'
+//         let url = baseURL + 'api/candidates?stage=QUEUE'        
+//         request          
+//           .get(url)        
+//           .set('Cookie', [cookieStr])
+//           .end((err, res) => {
+//             if (err) {
+//               reject(err)
+//             } else {
+//               console.log(err ? 'get candidates error: ' + err.toString() : '')               
+//               console.log('number of results: ' + res.body.length)              
+//               resolve(res.body)
+//             }
+//           })    
+//     })
+//     .then((candidates) => {      
+//       async.each(candidates, (candidate, callback) => {                                      
+//         console.log(candidate.lastName)
+//         // request
+//         //   .put(baseURL + 'api/candidates/' + candidate._id)
+//         //   .set('Cookie', [cookieStr])
+//         //   .send({ stage: 'QUEUE' })
+//         //   .end((err, result) => {
+//         //     if (err) {         
+//         //       console.log(err)
+//         //       callback(err)
+//         //     }          
+//         //     console.log(result.body.message)                                            
+//         //     callback()
+//         //   })                        
+//         callback(new Error())
+//       }, (err) => {
+//         if(err) {
+//           return done(err)
+//         }             
+//         done()
+//       })
+//     })
+//     .catch((err) => {
+//       console.log('error man')      
+//       console.log(err)      
+//     })
 
-    // getCandidates()      
-    //   .then((candidates) => {
-    //     console.log(candidates.length)
-    //     done()
-    //   })
-    //   .catch((err) => {
-    //     return done(err)
-    //   })
-
-
-  })
+//   })
 
 
 
-  // it('should be able to handle 100 list candidates requests', (done) => {    
+//   // it('should be able to handle 100 list candidates requests', (done) => {    
         
-  //   let url = 'http://54.241.231.143:3000/api/candidates'
+//   //   let url = 'http://54.241.231.143:3000/api/candidates'
         
-  //   console.log(cookieStr)
+//   //   console.log(cookieStr)
 
-  //   let times = 100
-  //   let cnt = 0
+//   //   let times = 100
+//   //   let cnt = 0
 
-  //   let list = setInterval(() => {
-  //     request
-  //       .get(url)         
-  //       .set('Cookie', [cookieStr])
-  //       .end((err, res) => {            
-  //         // Handle signin error        
-  //         console.log(err ? 'list candidate error: ' + err.toString() : '')               
-  //         cnt++
-  //         console.log('counter: ' + cnt)
-  //         if (cnt > times) {            
-  //           clearInterval(list);
-  //           return done()
-  //         }
-  //       })          
-  //   } ,1000); //1000 for every second
+//   //   let list = setInterval(() => {
+//   //     request
+//   //       .get(url)         
+//   //       .set('Cookie', [cookieStr])
+//   //       .end((err, res) => {            
+//   //         // Handle signin error        
+//   //         console.log(err ? 'list candidate error: ' + err.toString() : '')               
+//   //         cnt++
+//   //         console.log('counter: ' + cnt)
+//   //         if (cnt > times) {            
+//   //           clearInterval(list);
+//   //           return done()
+//   //         }
+//   //       })          
+//   //   } ,1000); //1000 for every second
 
-  // })
+//   // })
 
-  // afterEach((done) => {
-  //   User.remove().exec(() => {      
-  //     console.log('done!')
-  //     done()
-  //   })
-  // })
+//   // afterEach((done) => {
+//   //   User.remove().exec(() => {      
+//   //     console.log('done!')
+//   //     done()
+//   //   })
+//   // })
 
-  after((done) => {
-    console.log('all done')
-    // User.remove().exec(() => {      
-    //   console.log('done!')
-    done()
-    // })
-  })
+// //   after((done) => {
+// //     console.log('all done')
+// //     done()
+// //   })
 
 
-})
+// // })
--- modules/candidates/tests/server/candidate.server.routes.tests.js
+++ modules/candidates/tests/server/candidate.server.routes.tests.js
@@ -1,123 +1,227 @@
-// 'use strict'
+'use strict'
 
-// // let should = require('should')
-// const request = require('supertest')
-// const path = require('path')
-// const mongoose = require('mongoose')
-// const Candidate = mongoose.model('Candidate')
-// const Position = mongoose.model('Position')
-// const express = require(path.resolve('./config/lib/express'))
-// const random = require('random-name')
-// const async = require('async')
-// const should = require('should')
-// /**
-//  * Globals
-//  */
-// let app, agent, credentials, user, candidate, positions
+const request = require('supertest')      
+const path = require('path')
+const mongoose = require('mongoose')
+const fetch = require("node-fetch")
 
-// /**
-//  * Company routes tests
-//  */
-// describe('Candidate Load testing', () => {
+const Candidate = mongoose.model('Candidate')
+const Position = mongoose.model('Position')
+const express = require(path.resolve('./config/lib/express'))
+const random = require('random-name')
+const async = require('async')
+const should = require('should')
+
+const cookie = require('cookie');
+
+/**
+ * Globals
+ */
+let app, agent, credentials, user, candidate, positions, cookieStr
+
+const email1 = Date.now() + '@fakemailfsdfds.com'
+const email2 = Date.now() + '@faksfsdemailfsdfds.com'
+
+/**
+ * Company routes tests
+ */
+describe('Candidate Routes Testing', () => {
   
+  before((done) => {
+    // Get application
+    app = express.init(mongoose)
+    agent = request.agent(app)
+    Position.find().exec()
+      .then((res) => {        
+        positions = res        
+      })    
 
-//   before((done) => {
-//     // Get application
-//     app = express.init(mongoose)
-//     agent = request.agent(app)
-//     Position.find().exec()
-//       .then((res) => {        
-//         positions = res
-//         done()    
-//       })    
-//   })
-
-
-//   it('should be able to register 1000 candidates for position 1 from mobile', (done) => {    
+    // Sign in
+    credentials = {
+      email: 'skim@bernsinc.com',
+      password: 'g'
+    }
     
-//     let times = 1000
-//     let cnt = 0
-//     let complete = false
-
-//     let registration = setInterval(function register() {
-//       let email = Date.now() + '@fakemailfsdfds.com'
-//       let candidate = { 
-//         lastName: random.last(), 
-//         firstName: random.first() , 
-//         email: email,
-//         sms: '2136180615',
-//         position: positions[1],
-//         registeredFrom: 'MOBILE'
-//       }
-//       agent
-//         .post('/api/register/MOBILE')
-//         .send(candidate)
-//         .expect(200)
-//         .end((err, res) => {
-//           // Handle signin error        
-//           console.log(err ? 'create candidate error: ' + err.toString() : '')               
-//           cnt++
-//           console.log('counter: ' + cnt)
-//           if (cnt > times && !complete) {
-//             complete = true
-//             clearInterval(registration);
-//             return done()
-//           }
-//         })          
-//     } ,1000); //1000 for every second
-
-//   })
+    agent
+      .post('/api/auth/signin')
+      .send(credentials)    
+      .end((err, result) => {
+        if (err) {        
+          console.log('error signin in') 
+          console.log(err)
+          return done(err)
+        }              
+        let setcookie = result.headers["set-cookie"]           
+        console.log('cookie to string: ' + setcookie)
+        let cookies = cookie.parse(setcookie.toString()) 
+        cookieStr = 'sessionId=' + cookies.sessionId
+        console.log(cookieStr)          
+        done()
+      })      
+  })
 
 
-//   it('should be able to register 1000 candidates for position 2 from web', (done) => {    
+  it('should be able to register candidate for position 1 from mobile', (done) => {        
+
+    let candidate = { 
+      lastName: random.last(), 
+      firstName: random.first() , 
+      email: email1,
+      sms: '2136180615',
+      position: positions[1]
+    }
+
+    agent
+      .post('/api/register/MOBILE')
+      .send(candidate)
+      .expect(200)
+      .end((err, res) => {
+        if (err) {
+          console.log('create candidate error: ' + err.toString())               
+          return done(err)
+        }          
+        return done()
+      })          
+  })
+  
+  it('should be able to register candidate for position from web', (done) => {    
     
-//     let times = 1000
-//     let cnt = 0
-//     let complete = false
+    let candidate = { 
+      lastName: random.last(), 
+      firstName: random.first() , 
+      email: email2,
+      sms: '2136180615',
+      position: positions[2]     
+    }
+    agent
+      .post('/api/register/WEB')
+      .send(candidate)
+      .expect(200)
+      .end((err, res) => {
+        if (err) {
+          console.log('create candidate error: ' + err.toString())               
+          return done(err)
+        }          
+        return done()
+      })          
 
-//     let registration = setInterval(function register() {
-//       let email = Date.now() + '@fakemailfsdfds.com'
-//       let candidate = { 
-//         lastName: random.last(), 
-//         firstName: random.first() , 
-//         email: email,
-//         sms: '2136180615',
-//         position: positions[2],
-//         registeredFrom: 'WEB'
-//       }
-//       agent
-//         .post('/api/register/WEB')
-//         .send(candidate)
-//         .expect(200)
-//         .end((err, res) => {
-//           // Handle signin error        
-//           console.log(err ? 'create candidate error: ' + err.toString() : '')               
-//           cnt++
-//           console.log('counter: ' + cnt)
-//           if (cnt > times && !complete) {
-//             complete = true
-//             clearInterval(registration);
-//             return done()
-//           }
-//         })          
-//     } ,1000); //1000 for every second
+  })
 
-//   })
+  it('should be able to check in candidate who registered from web from web', (done) => {        
 
-//   // afterEach((done) => {
-//   //   User.remove().exec(() => {      
-//   //     console.log('done!')
-//   //     done()
-//   //   })
-//   // })
+    new Promise((resolve, reject) => {  
+      agent        
+        .get('/api/candidateByEmail/' + email2)
+        .expect(200)
+        .end((err, result) => {
+          if (err) {
+            console.log('create candidate error: ' + err.toString())               
+            reject(err)            
+          }                  
+          resolve(result.body)
+        })          
+    })
+    .then((candidate) => {
+      console.log(candidate.email)
+      agent        
+        .post('/api/checkin')
+        .send({ email: candidate.email })
+        .expect(200)
+        .end((err, result) => {
+          if (err) {
+            console.log('create candidate error: ' + err.toString())               
+            return done(err)            
+          }                  
+          return done()
+        })                
+    })    
+    .catch((err) => {
+      return done(err)
+    })
 
-//   after((done) => {
-//     console.log('all done')
-//     // User.remove().exec(() => {      
-//     //   console.log('done!')
-//     done()
-//     // })
-//   })
+  })
 
 
-// })
+  it('should be able update candidate to interview with department', (done) => {        
+
+    new Promise((resolve, reject) => {  
+      agent        
+        .get('/api/candidateByEmail/' + email1)
+        .expect(200)
+        .end((err, result) => {
+          if (err) {
+            reject(err)            
+          }                  
+          resolve(result.body)
+        })          
+    })
+    .then((candidate) => {      
+      agent        
+        .put('/api/candidates/' +  candidate._id)
+        .send({ stage: 'INTERVIEW' })
+        .set('Cookie', [cookieStr])
+        .expect(200)
+        .end((err, result) => {
+          if (err) {            
+            return done(err)            
+          }                  
+          return done()
+        })                
+    })    
+    .catch((err) => {
+      return done(err)
+    })
+
+  })
+
+
+  it('should be able update candidate to interview with department', (done) => {        
+
+    new Promise((resolve, reject) => {  
+      agent        
+        .get('/api/candidateByEmail/' + email1)
+        .expect(200)
+        .end((err, result) => {
+          if (err) {
+            reject(err)            
+          }                  
+          resolve(result.body)
+        })          
+    })
+    .then((candidate) => {      
+      agent        
+        .put('/api/candidates/' +  candidate._id)
+        .send({ stage: 'VALUATED' })
+        .set('Cookie', [cookieStr])
+        .expect(200)
+        .end((err, result) => {
+          if (err) {            
+            return done(err)            
+          }                  
+          return done()
+        })                
+    })    
+    .catch((err) => {
+      return done(err)
+    })
+
+  })
+
+
+  // afterEach((done) => {
+  //   User.remove().exec(() => {      
+  //     console.log('done!')
+  //     done()
+  //   })
+  // })
+
+  after((done) => {
+    console.log('all done')
+    // User.remove().exec(() => {      
+    //   console.log('done!')
+    done()
+    // })
+  })
+
+
+})
