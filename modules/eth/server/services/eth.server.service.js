9642f9b2a6c5b3e2966f3e8dd3d81b51ea15f677
scott kim <scott@scotts-Mac-mini.local>
2018-10-09 16:19:57 -0700

    gas fix

--- modules/eth/server/services/eth.server.service.js
+++ modules/eth/server/services/eth.server.service.js
@@ -351,8 +351,9 @@ exports.set2xStakeBySeller = function(arbitrationId, seller) {
   let trx = {
       from: seller,
       gasPrice: defaultGas.gasPrice,   //maximum price per gas
-      gas: defaultGas.gasPrice //max number of gas to be used      
+      gas: defaultGas.gas //max number of gas to be used      
   }
+
 
   let idHex = prepend0x(arbitrationId.toString())
   
