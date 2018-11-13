7255f8c6da471bfeefefbba124feb4ed628f337f
scott kim <scott@scotts-Mac-mini.local>
2018-11-13 14:35:42 -0800

    styles for asset detail

--- modules/assets/server/routes/assets.server.routes.js
+++ modules/assets/server/routes/assets.server.routes.js
@@ -3,22 +3,22 @@
 /**
  * Module dependencies
  */
-var assetsPolicy = require('../policies/assets.server.policy'),
-  assets = require('../controllers/assets.server.controller'),
-  path = require('path'),
-  Eth = require(path.resolve('./modules/eth/server/controllers/eth.server.controller'))  
+const assetsPolicy = require('../policies/assets.server.policy')
+const assets = require('../controllers/assets.server.controller')
+const path = require('path')
+const Eth = require(path.resolve('./modules/eth/server/controllers/eth.server.controller'))  
 
 module.exports = function(app) {
   // assets Routes
   app.route('/api/assets').all(assetsPolicy.isAllowed)
     .get(assets.getAllAssetsFromContract, assets.bindTitleAndDescription, assets.list)
-    .post(Eth.unlockAccount, assets.create);
+    .post(Eth.unlockAccount, assets.create)
 
   app.route('/api/assets/:assetId').all(assetsPolicy.isAllowed)
     .get(assets.read, assets.bindMarketPlaces)
     .put(assets.update)
-    .delete(assets.delete);
+    .delete(assets.delete)
 
   // Finish by binding the asset middleware
-  app.param('assetId', assets.assetByID);
-};
+  app.param('assetId', assets.assetByID)
+}
