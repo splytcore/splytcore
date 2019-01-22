49306c69e40ace91e2b81ddb54bf740c4993fc2b
scott kim <scott@scotts-Mac-mini.local>
2019-01-21 18:42:01 -0800

    initial list assets by category

--- modules/assets/client/controllers/list-assets.client.controller.js
+++ modules/assets/client/controllers/list-assets.client.controller.js
@@ -8,6 +8,7 @@
   AssetsListController.$inject = ['AssetsService', 'EthService', '$state', 'Authentication', '$cookies'];
 
   function AssetsListController(AssetsService, EthService, $state, Authentication, $cookies) {
+    
     var vm = this;
     vm.listType = $state.current.name.toString()
     vm.assets = AssetsService.query({ listType: vm.listType })
