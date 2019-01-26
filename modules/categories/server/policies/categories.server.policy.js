85cb6fb146ca1794db9b43bf033a499a6c79b042
scott kim <scott@scotts-Mac-mini.local>
2019-01-25 16:04:57 -0800

    added update category

--- modules/categories/server/policies/categories.server.policy.js
+++ modules/categories/server/policies/categories.server.policy.js
@@ -28,7 +28,7 @@ exports.invokeRolesPolicies = function () {
       permissions: ['get', 'post']
     }, {
       resources: '/api/categories/:categoryId',
-      permissions: ['get']
+      permissions: ['get', 'put']  
     }]
   }, {
     roles: ['guest', 'customer'],
