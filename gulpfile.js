a4633b3cc2e8d6687eeec385a28cb22ad54f88c6
scott kim <scott@scotts-Mac-mini.local>
2019-01-30 16:11:43 -0800

    jshitn enabled for dev

--- gulpfile.js
+++ gulpfile.js
@@ -294,7 +294,7 @@ gulp.task('test', function (done) {
 
 // Run the project in development mode
 gulp.task('default', function (done) {
-  runSequence('env:dev', ['nodemon', 'watch'], done)
+  runSequence('env:dev', 'lint',['nodemon', 'watch'], done)
 })
 
 // Run the project in debug mode
