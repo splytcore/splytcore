3cd2f3ccf4f687726405e6838bd00eddd5655137
lenovo <dhru_7777@yahoo.com>
2019-02-06 16:13:01 -0800

    updates to views

--- modules/carts/server/routes/carts-items.server.routes.js
+++ modules/carts/server/routes/carts-items.server.routes.js
@@ -3,13 +3,16 @@
 /**
  * Module dependencies
  */
+const path = require('path')
 const cartsPolicy = require('../policies/carts.server.policy')
 const cartsItems = require('../controllers/carts-items.server.controller')
+const assets = require(path.resolve('./modules/assets/server/controllers/assets.server.controller'))
 
 module.exports = function(app) {
   // Carts Routes
   app.route('/api/cartsitems')
     .get(cartsItems.list)
+
     .post(cartsItems.create)
 
   app.route('/api/cartsitems/:cartItemId')
