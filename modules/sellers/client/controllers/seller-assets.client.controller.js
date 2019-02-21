a0ba6ebb3dabe6c115c6059998ef33db82b2ead3
scott kim <scott@scotts-Mac-mini.local>
2019-02-20 18:28:35 -0800

    change api for client

--- modules/sellers/client/controllers/seller-assets.client.controller.js
+++ modules/sellers/client/controllers/seller-assets.client.controller.js
@@ -5,13 +5,18 @@
     .module('sellers')
     .controller('SellerAssetsController', SellerAssetsController)
 
-  SellerAssetsController.$inject = ['AssetsService', '$state', 'Authentication']
+  SellerAssetsController.$inject = ['$http', '$state', 'Authentication']
 
-  function SellerAssetsController(AssetsService, $state, Authentication) {
+  function SellerAssetsController($http, $state, Authentication) {
     var vm = this
     vm.user = Authentication.user
 
-    vm.assets = AssetsService.query({ user: vm.user._id })
-  
+    $http.get('api/assets/mine')
+      .then((result) => {
+        vm.assets = result.data
+      })
+      .catch((err) => {
+        alert(err)
+      })
   }
 }())
