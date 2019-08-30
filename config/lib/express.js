1a46f39a2f2859ab39548be5f5dae3971d6b73c0
dhru_7777 <dhru_7777@yahoo.com>
2019-08-29 21:37:18 -0700

    able to iframe for shopify

--- config/lib/express.js
+++ config/lib/express.js
@@ -140,7 +140,8 @@ module.exports.initModulesConfiguration = function (app, db) {
 module.exports.initHelmetHeaders = function (app) {
   // Use helmet to secure Express headers
   var SIX_MONTHS = 15778476000;
-  app.use(helmet.xframe());
+  //remove xframe to enable iframe capibilities
+  //app.use(helmet.xframe());
   app.use(helmet.xssFilter());
   app.use(helmet.nosniff());
   app.use(helmet.ienoopen());
